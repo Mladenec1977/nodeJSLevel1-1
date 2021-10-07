dafd04bd66787eb927817fb65edd59376bf1e1e5
Mladenec1977 <rozgon1977@gmail.com>
2021-10-08 00:53:56 +0300

    Level start1

--- aroz.js
+++ aroz.js
@@ -25,6 +25,16 @@ function Product (name = "",
     this.setDescription = function (description) {
         this.description = description;    
     }
+    this.getPrice = function () {return this.price;}
+    this.setPrice = function (price) {this.price = price;}
+    this.getBrand = function () {return this.brand;}
+    this.setBrand = function (brand) {this.brand = brand;}
+    this.getSize = function () {return this.activeSize;}
+    this.setSize = function (size) {this.activeSize = size;}
+    this.getQuantity = function () {return this.quantity;}
+    this.setQuantity = function (quantity) {this.quantity = quantity;}
+    this.getDane = function () {return this.date;}
+    this.setDate = function (date = new Date) {this.date = date;}
     this.getReviewByID = function (id) {
         if(id >=0 & id < this.reviews.length) {
             return this.reviews[id];
@@ -54,29 +64,47 @@ function Product (name = "",
     this.getAverageRating = function () {
         let resAr = [];
         this.reviews.forEach(element => {
-            resAr = Object.values(element.rating);
+            resAr = resAr.concat(Object.values(element.rating));
         });
+        console.log(resAr);
         let check = resAr.reduce((a, b) => a + b) / resAr.length;
         return check;
     }
 }
 // Obj - Reviews
-function Reviews (Id, author = "", 
+function Reviews (id, 
+                  author = "", 
                   date = new Date, 
                   comment = ""
                   ){
-    this.ID = '' + Id;
+    this.ID = '' + id;
     this.author = author;
     this.date = date;
     this.comment = comment;
-    this.rating = [];
+    this.rating = {};
 }
 
-test1 = new Product("BMV333");
-console.log(test1.name);
-test1.name = 'BMV';
-console.log(test1.name);
-// test1.getName();
-// console.log(test1.getID());
-// test1.deleteSize(-10);
-// console.log(test1.sizes);
+// test
+let testPro1 = new Product("BMV333");
+console.log(testPro1.name);
+testPro1.name = 'BMV';
+console.log(testPro1.getName());
+console.log(testPro1.getID());
+console.log(testPro1.sizes);
+testPro1.deleteSize(2);
+console.log(testPro1.sizes);
+
+let ratArr1 = {service: 5, price: 10, value: 34, quality: 12};
+let ratArr2 = {price: 18, quality: 30};
+let ratArr3 = {service: 32, price: 20, value: 13, quality: 12};
+
+let testRew1 = new Reviews (1, 'Nikitenko');
+let testRew2 = new Reviews (2, 'Bro');
+let testRew3 = new Reviews (3, 'Redly');
+testRew1.rating = ratArr1;
+testRew2.rating = ratArr2;
+testRew3.rating = ratArr3;
+testPro1.addReview(testRew1);
+testPro1.addReview(testRew2);
+testPro1.addReview(testRew3);
+console.log(testPro1.getAverageRating());
