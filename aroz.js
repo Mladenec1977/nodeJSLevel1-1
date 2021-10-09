321a22aef4b21fa367c97550045fcba5d1c02482
Mladenec1977 <rozgon1977@gmail.com>
2021-10-09 22:08:00 +0300

    Level end1-1

--- aroz.js
+++ aroz.js
@@ -1,12 +1,18 @@
 let count = 0;
 
-// Obj - Product
+/**
+ * Obj - Product
+ * @param {*} name 
+ * @param {*} description 
+ * @param {*} price 
+ * @param {*} brand 
+ */
 function Product (name = "", 
                   description = "", 
                   price = 0.00,
                   brand = ""
                   ){
-    this.ID = Symbol(`${count++}`);
+    this.ID = count++;
     this.name = name;    
     this.description = description;
     this.price = price;
@@ -33,7 +39,7 @@ function Product (name = "",
     this.setSize = function (size) {this.activeSize = size;}
     this.getQuantity = function () {return this.quantity;}
     this.setQuantity = function (quantity) {this.quantity = quantity;}
-    this.getDane = function () {return this.date;}
+    this.getDate = function () {return this.date;}
     this.setDate = function (date = new Date) {this.date = date;}
     this.getReviewByID = function (id) {
         if(id >=0 & id < this.reviews.length) {
@@ -71,7 +77,14 @@ function Product (name = "",
         return check;
     }
 }
-// Obj - Reviews
+
+/**
+ * Obj - Reviews
+ * @param {*} id 
+ * @param {*} author 
+ * @param {*} date 
+ * @param {*} comment 
+ */
 function Reviews (id, 
                   author = "", 
                   date = new Date, 
@@ -83,16 +96,49 @@ function Reviews (id,
     this.comment = comment;
     this.rating = {};
 }
+/**
+ * Product search
+ * @param {*} products - array of goods
+ * @param {*} search - search term
+ * @returns 
+ */
+function searchProducts(products, search) {
+    let res = [];
+    search = search.toLowerCase();
+    if (search.indexOf("*") > 0) {
+        let resSear = search.split('*');
+        search = resSear[0];        
+    }
+    for (let item of products){
+        if (item.name.toLowerCase().indexOf(search) >= 0) {            
+            res.push(item.name);
+        }
+        if (item.description.toLowerCase().indexOf(search) >= 0) {            
+            res.push(item.description);
+        }
+    }
+    return res;
+}
+/**
+ * Returns an array by sorting condition
+ * @param {*} products - an array to sort
+ * @param {*} sortRule - sorting condition
+ * @returns Returns an array by sorting condition
+ */
+function sortProducts(products, sortRule) {
+    let res = products.sort((a, b) => (a[sortRule] > b[sortRule])? 1: -1);
+    return res;
+}
 
 // test
 let testPro1 = new Product("BMV333");
 console.log(testPro1.name);
 testPro1.name = 'BMV';
-console.log(testPro1.getName());
-console.log(testPro1.getID());
-console.log(testPro1.sizes);
+// console.log(testPro1.getName());
+// console.log(testPro1.getID());
+// console.log(testPro1.sizes);
 testPro1.deleteSize(2);
-console.log(testPro1.sizes);
+// console.log(testPro1.sizes);
 
 let ratArr1 = {service: 5, price: 10, value: 34, quality: 12};
 let ratArr2 = {price: 18, quality: 30};
@@ -107,4 +153,14 @@ testRew3.rating = ratArr3;
 testPro1.addReview(testRew1);
 testPro1.addReview(testRew2);
 testPro1.addReview(testRew3);
-console.log(testPro1.getAverageRating());
+// console.log(testPro1.getAverageRating());
+
+let testPro2 = new Product("Мяч", "Мяч для игр в футбол", 24.30);
+let testPro3 = new Product("Футболка", "Футболка легкая, синего цвета", 35.10);
+let testPro4 = new Product("Короткие футболки", "Пляжные футболки", 12.50);
+let testPro5 = new Product("Тетради", "Тетрадь для записей в клеточку", 10);
+let arrPro = [testPro1, testPro2, testPro3, testPro4, testPro5];
+let testSearch = "футбол";
+let testSearch2 = "уТбО*";
+// console.log(searchProducts(arrPro, testSearch2));
+// console.log(sortProducts(arrPro, 'price'));
