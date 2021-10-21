ef64eeb33309e31827dfedca0ee6905ff99454b2
Mladenec1977 <rozgon1977@gmail.com>
2021-10-22 00:21:04 +0300

    Leve-1-2-4

--- level1x2x3.js
+++ level1x2x3.js
@@ -38,26 +38,95 @@ function processHttpRequest($method, $uri, $headers, $body) {
            let arrNums = arrSum[1].split('?nums=');
            if (arrNums.length > 1) {
                $body = arrNums[1].split(',').reduce((a, b) => a + b);
+               end();
+               return;
            } else {
                statusCode = '400';
                statusMessage = 'Bad Request';
                $body = 'bad Request';
+               end();
+               return;
            }
            
        } else {
            statusCode = '404';
            statusMessage = 'Not Found';
            $body = 'not found';
+           end();
+           return;
        }
        
+   } else if ($method == 'POST') {
+       let check = false;
+       if ($uri == '/api/checkLoginAndPassword') {
+           for (let item of $headers) {
+               if (item[0] == 'Content-Type' && item[1] == 'application/x-www-form-urlencoded') {
+                   check = true;
+                   break;
+               }
+           }
+       }
+       if (check) {
+           const fs = require("fs");
+           let file = 'passwords.txt';
+           fs.readFile(file, "utf8", function(error,data){
+               if (error) {
+                statusCode = '500';
+                statusMessage = 'Internal Server Error';
+                $body = 'internal Server Error';
+                end();
+                return;
+               }
+               if (data) {
+                   let arrLogData = data.split('\n');
+                   let login = $body.split('&')[0].split('=')[1];
+                   let password = $body.split('\n')[0].split('&')[1].split('=')[1];                   
+                   for (let item of arrLogData) {
+                       let arrItem = item.split(':');
+                       let loginItem = arrItem[0];
+                       let passItem = arrItem[1];                       
+                       if (loginItem == login && passItem == password) {
+                           $body = '<h1 style="color:green">FOUND</h1>';                        
+                           end();
+                           return;
+                       }
+                   }
+                    statusCode = '401';
+                    statusMessage = 'Unauthorized';
+                    $body = 'unauthorized';
+                    end();
+                    return;                   
+               }
+           });
+       } else {
+            statusCode = '400';
+            statusMessage = 'Bad Request';
+            $body = 'bad Request';
+            end();
+            return;
+       }
    } else {
         statusCode = '400';
         statusMessage = 'Bad Request';
         $body = 'bad Request';
+        end();
+        return;
    }
-   $headers.push(['Content-Length', '' + $body.length]);
+   function end() {
+       let checkHed = true;
+   for (let item of $headers) {
+        if (item[0] == 'Content-Length') {
+            item[1] = '' + $body.length;
+            checkHed = false;
+            break;
+        }
+    }
+    if (checkHed) {
+        $headers.push(['Content-Length', '' + $body.length]);
+    }
 
    outputHttpResponse(statusCode, statusMessage, $headers, $body);
+   }   
 }
    
 function parseTcpStringAsHttpRequest($string) {
